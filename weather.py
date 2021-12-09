92cbea8cc01c20c03eb44dee94501c9ae78cbbd5
科君 <kejun.hch@qq.com>
2021-12-09 20:59:39 +0800

    fix: weather

--- weather.py
+++ weather.py
@@ -15,40 +15,55 @@ import requests
 from bs4 import BeautifulSoup
 
 # 温度 气象 空气 指数
-template = '''温度: {}
+TEMPLATE = '''温度: {}
 气象: {} 
 空气指数: {}
-穿衣指数: {}
-紫外线指数: {}
+{}
 '''
 
 
 class WeatherScraper:
     def query_weather_qq(self, province, city):
         try:
-            url = f'https://wis.qq.com/weather/common?source=pc&province={province}&city={city}&weather_type=forecast_24h|air|tips|index&_={int(round(time.time() * 1000))}'
+            url = f'https://wis.qq.com/weather/common?source=pc&province={province}&city={city}&weather_type=forecast_24h|air|tips|index&_={int(time.time() * 1000)}'
             resp = requests.get(url)
             data = json.loads(resp.text)['data']
             forecast = data['forecast_24h']['1']
             air = data['air']
             index = data['index']
-            result = template.format(forecast['min_degree'] + '°C ~ ' + forecast['max_degree'] + '°C',
-                                     forecast['day_weather'] + '转' + forecast['night_weather'],
-                                     str(air['aqi']) + '(' + air['aqi_name'] + ')',
-                                     index['clothes']['info'] + '，' + self.query_wearing_index(city),
-                                     index['ultraviolet']['detail'])
+            result = TEMPLATE.format(
+                forecast['min_degree'] + '°C ~ ' + forecast['max_degree'] + '°C',  # 温度
+
+                forecast['day_weather'] + '转' + forecast['night_weather']  # 气象
+                if forecast['day_weather'] != forecast['night_weather']
+                else forecast['day_weather'],
+
+                str(air['aqi']) + '(' + air['aqi_name'] + ')',  # 空气指数
+
+                self.query_indices(city)  # 其他指数
+            )
             return result
         except Exception as e:
             logging.error(f"failed to query weather: {e}")
 
-    def query_wearing_index(self, city):
+    def query_indices(self, city):
+        result = []
         url = 'http://www.weather.com.cn/weather1d/' + self._parse_place(city) + '.shtml'
         resp = requests.get(url)
         resp.encoding = 'utf-8'
         # print(resp.text)
         soup = BeautifulSoup(resp.content, 'lxml')
-        wearing_index = soup.find("li", id="chuanyi")
-        return wearing_index.select('a p')[0].string
+        index_ul = soup.find("li", id="chuanyi").parent
+        index_wl = ['感冒指数', '穿衣指数']
+
+        for index_li in index_ul.find_all(recursive=False):
+            index_name = index_li.select('em')
+            if index_name and index_name[0].string in index_wl:
+                # print(type(index.select('em')[0]))
+                result.append(
+                    f"{index_name[0].string}: {index_li.select('span')[0].string}，{index_li.select('p')[0].string}")
+
+        return '\n'.join(result)
 
     def query_weather(self, city):
         result = ''
@@ -81,7 +96,7 @@ class WeatherScraper:
             # print(aqi)
 
             # result = {"h0": temp_high, "l0": temp_low, "w0": weather}
-            result = template.format(temp_low + '°C ~ ' + temp_high + '°C', weather, self.query_air(city), None, None)
+            result = TEMPLATE.format(temp_low + '°C ~ ' + temp_high + '°C', weather, self.query_air(city), None, None)
         except Exception as e:
             logging.error(f"failed to query weather: {e}")
         return result
@@ -113,6 +128,6 @@ class WeatherScraper:
 
 if __name__ == '__main__':
     w = WeatherScraper()
-    print(w.query_wearing_index('上海'))
+    print(w.query_indices('上海'))
     # w.query_air('北京')
     # w.query_weather_qq('上海市', '上海市')
